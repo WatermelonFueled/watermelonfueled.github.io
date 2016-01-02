f1a717ee894795ce571d18d429922965a20c0327
David Park <david@watermelonfueled.com>
2016-01-02 14:36:21 -0500

    prototype watermelon canvas drawing functios

--- scripts/application.js
+++ scripts/application.js
@@ -30,6 +30,36 @@ $(document).ready(function() {
     //blade detail
   }
   //canvas watermelon
+  //front half
+  var watermelonFrontCanvas = document.getElementById("watermelon-front");
+  watermelonFrontCanvas.width = 280;
+  watermelonFrontCanvas.height = 280;
+  if (watermelonFrontCanvas.getContext){
+    var front = watermelonFrontCanvas.getContext('2d');
+    front.fillStyle = "#25791b";
+    squiglyFill(front, 10, 4, 100,50,100,200);
+  }
+
+  function squiglyFill(context, bulge, count, startX, startY, endX, endY) {
+    context.beginPath();
+    context.moveTo(startX, startY);
+    var stepY = ( endY - startY ) / count;
+    var stepX = ( endX - startX ) / count;
+    var bulgeX = Math.atan2(stepY, stepX)/(Math.PI/2)*bulge;
+    var bulgeY = bulge - bulgeX;
+    for (x = startX + stepX, y = startY + stepY, i=0; i<count; i++, x+=stepX, y+=stepY) {
+      context.quadraticCurveTo(Math.round(x-stepX/2),Math.round(y-stepY/2),Math.round(x-stepX/2+bulgeX) ,Math.round(y-stepY/2+bulgeY));
+      context.quadraticCurveTo(Math.round(x-stepX/2),Math.round(y-stepY/2),Math.round(x), Math.round(y));
+    }
+    context.fill();
+    for (x = endX - stepX, y = endY - stepY, i=0; i<count; i++, x-=stepX, y-=stepY) {
+      context.quadraticCurveTo(Math.round(x+stepX/2),Math.round(y+stepY/2),Math.round(x+stepX/2-bulgeX) ,Math.round(y+stepY/2-bulgeY));
+      context.quadraticCurveTo(Math.round(x+stepX/2),Math.round(y+stepY/2),Math.round(x), Math.round(y));
+    }
+    context.fill();
+    context.closePath();
+
+  }
 
   //header window scroll shrink
   $(window).on('scroll', function(){ $('#main-menu-nav').toggleClass('small', $(document).scrollTop() > 0); });
